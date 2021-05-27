ccdf4b90a2f9e1d2155deb8d492d1bf85da9a86c
Lrd134 <larryc3200@gmail.com>
2021-05-27 16:05:54 -0400

    commit tree 793ca6893222fa8492ab99068fa11b8b4c153739

--- app.rb
+++ app.rb
@@ -1,9 +1,23 @@
 require './environment'
-
+require_relative './app/models/pirate'
+require_relative './app/models/ship'
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+    get '/new' do
+      erb :"pirates/new"
+    end
+    post '/pirates' do
+      @pirate = Pirate.new(params)
+      Ship.new(params["pirate"]["ships"][0])
+      Ship.new(params["pirate"]["ships"][1])
+      erb :"pirates/show"
+      # raise params.inspect
 
+    end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  @@all = []
+  def initialize(args)
+    @name = args["pirate"]["name"]
+    @weight = args["pirate"]["weight"]
+    @height = args["pirate"]["height"]
+    save
+  end
+  def save
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def display
+    "Pirate Name: #{@name}\nWeight: #{@weight}\nHeight: #{@height}"
+  end
 end
